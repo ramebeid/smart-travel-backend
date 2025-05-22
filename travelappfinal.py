267dad2c80fb671bbe474bf147d34b6b7a2e0339
Ramy Ebeid <ramyebeid@Ramys-Air.fritz.box>
2025-05-22 21:32:00 +0200

    Fix: defined GOOGLE_PLACES_API_KEY from env

--- travelappfinal.py
+++ travelappfinal.py
@@ -9,6 +9,8 @@ from smart_filter import filter_places, inject_must_see_places
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
+GOOGLE_PLACES_API_KEY = os.getenv("GOOGLE_PLACES_API_KEY")
+
 
 app = Flask(__name__)
 CORS(app)
